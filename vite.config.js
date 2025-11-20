fddf71e1b7bf5cf68f3bf8fc3ae38afa1cf06132
Anuphong ตัวปลอม <jp0951269346@gmail.com>
2025-11-20 17:56:32 +0700

    vite-config-fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  // เพิ่มส่วนนี้เพื่อแก้ Warning
+  build: {
+    chunkSizeWarningLimit: 1600,
   },
 })
